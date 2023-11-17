a2c833436027ff741c8aa311de73cad2e4e3008d
Lucas Oliveira <lucas.apclube@gmail.com>
2023-11-16 23:38:45 -0300

    motivation

---
+++ app/src/main/java/com/example/motivation/infra/SecurityPreferences.kt
@@ -0,0 +1,21 @@
+package com.example.motivation.infra
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class SecurityPreferences(context: Context) {
+
+    private val security: SharedPreferences =
+
+        context.getSharedPreferences("Motivation", Context.MODE_PRIVATE)
+
+    fun storeString(key: String, str: String) {
+        security.edit().putString(key, str).apply()
+    }
+
+    fun getString(key: String): String {
+        security.getString(key, "")
+
+        return security.getString(key, "")?:""
+    }
+}
